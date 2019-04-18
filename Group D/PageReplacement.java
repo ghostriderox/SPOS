12778e6f2d8d7540bc2ea785bd9a2fedc752f8f1
Jay Janodia <jayjanodia@localhost.localdomain>
2019-04-18 17:02:27 +0530

    .; : '

--- Group D/PageReplacement.java
+++ Group D/PageReplacement.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class PageReplacement {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1: FCFS \n2: LRU \n3: Optimal");
+		int choice = sc.nextInt();
+		switch(choice){
+			case 1:
+			
 		FCFS fcfs=new FCFS();
-		//fcfs.execute();
-		
+		fcfs.execute();
+		break;
+			case 2:
+			
 		LRU lru=new  LRU();
-		//lru.execute();
-		
+		lru.execute();
+		break;
+			case 3:
+			
 		Optimal optimal=new Optimal();
 		optimal.execute();
+		break;
+		}
 	}
 
 }
